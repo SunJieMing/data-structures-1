970581e13a5342eea30e9825c5aa8abbba9c9713
Ben Nelson <benn@ifit.com>
2016-02-09 20:31:50 -0700

    Included stack solution

--- spec/specs.js
+++ spec/specs.js
@@ -9,13 +9,47 @@ describe('stack', function() {
   });
   it('reports a size of 1 after adding a value', function() {
     stack.add(null);
-    expect(stack.size()).to.equal(0);
+    expect(stack.size()).to.equal(1);
   });
-  it('should be able to remove values', function() {
+  it('should be able to remove a value', function() {
     stack.add(true);
     var returnValue = stack.remove();
     expect(returnValue).to.equal(true);
     expect(stack.size()).to.equal(0);
   });
+  it('should not be built using an array', function() {
+    for (var key in stack) {
+      if (stack.hasOwnProperty(key)) {
+          expect(Array.isArray(stack[key])).to.equal(false);
+      }
+    }
+  });
+});
 
+describe('queue', function() {
+  var stack;
+
+  beforeEach(function() {
+    stack = new Stack();
+  });
+  it('reports a size of 0 for a new stack', function() {
+    expect(stack.size()).to.equal(0);
+  });
+  it('reports a size of 1 after adding a value', function() {
+    stack.add(null);
+    expect(stack.size()).to.equal(1);
+  });
+  it('should be able to remove a value', function() {
+    stack.add(true);
+    var returnValue = stack.remove();
+    expect(returnValue).to.equal(true);
+    expect(stack.size()).to.equal(0);
+  });
+  it('should not be built using an array', function() {
+    for (var key in stack) {
+      if (stack.hasOwnProperty(key)) {
+          expect(Array.isArray(stack[key])).to.equal(false);
+      }
+    }
+  });
 });
--- src/stack.js
+++ src/stack.js
@@ -1,7 +1,20 @@
 var Stack = function() {
-
+  this.storage = {};
+  this.length = 0;
 };
 
-Stack.prototype.size = function() {};
-Stack.prototype.add = function() {};
-Stack.prototype.remove = function() {};
+Stack.prototype.size = function() {
+  return this.length;
+};
+
+Stack.prototype.add = function(input) {
+  this.storage[this.length] = input;
+  this.length++;
+};
+
+Stack.prototype.remove = function() {
+  this.length && this.length--;
+  var returnValue = this.storage[this.length];
+  delete this.storage[this.length];
+  return returnValue;
+};
